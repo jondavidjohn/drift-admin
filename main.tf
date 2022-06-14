7f4a08d4c5b446907ad735f916bbfb36cf3ae358
Jonathan Johnson <me@jondavidjohn.com>
2022-06-14 17:46:57 -0500

    init commit

---
+++ main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    tfe = {
+      version  = "~> 0.26.0"
+    }
+  }
+}
+
+provider "tfe" {
+  hostname = var.hostname
+}
+
+variable "hostname" {
+  type = string
+}
+
+variable "user_email" {
+  type = string
+}
+
+resource "tfe_organization" "drift_testing_org" {
+  name  = "drift-testing-org"
+  email = var.user_email
+}
+
+data "tfe_organization_membership" "user" {
+  organization  = tfe_organization.drift_testing_org.name
+  email = var.user_email
+}
+
+resource "tfe_workspace" "managed_workspace" {
+  count        = 5
+  organization = tfe_organization.drift_testing_org.name
+  auto_apply   = true
+  name         = "managed-workspace-${count.index}"
+}
