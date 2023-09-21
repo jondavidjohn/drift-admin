a7ca1a32251900cb986dcb9c8c737ca39e320185
Jonathan D. Johnson <me@jondavidjohn.com>
2023-09-21 12:37:46 -0500

    Update checks.tf

--- checks.tf
+++ checks.tf
@@ -1,6 +1,6 @@
 check "check_workspace_name" {
   assert {
-    condition     = tfe_workspace.managed_workspace.name == "managed-workspace"
+    condition     = tfe_workspace.managed_workspace.name == "managed-workspace-update-2"
     error_message = "Workspace ${tfe_workspace.managed_workspace.name} does not have the correct name"
   }
 }
