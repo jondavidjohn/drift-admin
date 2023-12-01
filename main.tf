39c25e873d0fcc33fb5568574eea7e0ebcd27841
Jonathan D. Johnson <me@jondavidjohn.com>
2023-12-01 12:13:21 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,12 +21,12 @@ provider "tfe" {
 resource "tfe_workspace" "managed_workspace" {
   organization = var.org_name
   auto_apply   = false
-  name         = "managed-workspace-update-2"
+  name         = "managed-workspace-update-3"
   tag_names    = ["some:tag", "another:tag"]
 
   lifecycle {
     postcondition {
-      condition     = self.name == "managed-workspace-update-2"
+      condition     = self.name == "managed-workspace-update-3"
       error_message = "This workspace does not have the correct name"
     }
   }
