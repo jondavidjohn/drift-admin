2573c18dfa46713c29fd778eb4f49fb94858247a
Jonathan Johnson <me@jondavidjohn.com>
2024-04-10 17:39:18 -0500

    update check

--- checks.tf
+++ checks.tf
@@ -1,6 +1,6 @@
 check "check_workspace_name" {
   assert {
-    condition     = tfe_workspace.managed_workspace.name == "managed-workspace-update-2"
+    condition     = tfe_workspace.managed_workspace.name == "managed-workspace-update-3"
     error_message = "Workspace ${tfe_workspace.managed_workspace.name} doesn't have the correct name"
   }
 }
